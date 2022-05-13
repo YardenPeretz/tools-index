26a62713f9620ec737458d638215f0f5abd5b38c
kachok1213 <amiperisr@gmail.com>
2022-05-13 22:21:21 +0300

    synced

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 
 import './App.css';
-<<<<<<< HEAD
+
 import {Container,Row,Col} from 'react-bootstrap/';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Container>
         <Row>
+        <Header></Header>
           <Col>1 of 2</Col>
           <Col>2 of 2</Col>
         </Row>
+        <Footer></Footer>
       </Container>
       
-=======
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <Footer></Footer>
->>>>>>> f945ac87e63d41b64d487b34b6232c26d96de2e5
+
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,13 @@ import './Header.css'
 
 function Header() {
   return (
-<<<<<<< HEAD
+
     <div className="App">
       
-=======
-    <div className="Header">
+
+        <div className="Header">
         <div className="navbar navbar-light bg-light">test header</div>
->>>>>>> f945ac87e63d41b64d487b34b6232c26d96de2e5
+       </div>
     </div>
   );
 }
