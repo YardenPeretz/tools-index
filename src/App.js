e60eebe1a92f503a7b2643988fff57f42f750d65
kachok1213 <amiperisr@gmail.com>
2022-05-13 22:12:57 +0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import {Container,Row,Col} from 'react-bootstrap/';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      HELLO WORLD!
+      <Container>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
+      
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+
+
 function Header() {
   return (
     <div className="App">
-      HELLO WORLD!
+      
     </div>
   );
 }
