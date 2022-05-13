15f4a712b8703067fcf2d3789fc41eb610024421
kachok1213 <amiperisr@gmail.com>
2022-05-13 23:43:54 +0300

    container amit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import './Home.css';
 
-import {Container,Row,Col} from 'react-bootstrap/';
+import { Container, Row, Col } from 'react-bootstrap/';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
@@ -8,8 +8,15 @@ import Footer from '../Footer/Footer';
 function Home() {
   return (
     <div className="App">
+      <Container>
         <Header></Header>
+        <Row>
+        <Col className='col'>col number 1</Col>
+          <Col>col number 2</Col>
+          <Col>col number 3</Col>
+        </Row>
         <Footer></Footer>
+      </Container>
     </div>
   );
 }
